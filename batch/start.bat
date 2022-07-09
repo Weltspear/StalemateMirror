48f8ef4035747e1807595616260a793c891a2467
Weltspear <>
2022-07-09 17:52:44 +0200

    Removed bash and batch scripts

--- batch/start.bat
+++
@@ -1,1 +0,0 @@
-java -cp lib/annotations.jar;lib/jackson-annotations.jar;lib/jackson-core.jar;lib/jackson-databind.jar;lib/jackson-dataformat-yaml.jar;lib/snakeyaml.jar;lib/panic.jar;Stalemate.jar net.stalemate.Main

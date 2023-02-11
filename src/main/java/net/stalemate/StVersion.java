69e8f0aeba944b8e8b0c4f984ae1dabd50ef4b4b
Weltspear <>
2023-02-11 15:48:46 +0100

    Increased version number

--- src/main/java/net/stalemate/StVersion.java
+++ src/main/java/net/stalemate/StVersion.java
@@ -19,6 +19,6 @@
 package net.stalemate;
 
 public class StVersion {
-    public static final String version = "v0.3.xa-dev";
-    public static final int packet_version = 9;
+    public static final String version = "v0.3.40a";
+    public static final int packet_version = 10;
 }

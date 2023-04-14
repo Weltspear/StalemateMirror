7ee0840b8b0492607eb47647df9d2ca3b14a2286
Weltspear <>
2023-04-14 22:36:02 +0200

    Now TransportPlane can load and deploy ground units.

--- src/main/java/net/stalemate/server/core/units/air/TransportPlane.java
+++ src/main/java/net/stalemate/server/core/units/air/TransportPlane.java
@@ -22,13 +22,108 @@ import net.stalemate.server.core.AirUnit;
 import net.stalemate.server.core.Unit;
 import net.stalemate.server.core.animation.Animation;
 import net.stalemate.server.core.animation.AnimationController;
-import net.stalemate.server.core.buttons.BombButton;
 import net.stalemate.server.core.buttons.MoveButton;
 import net.stalemate.server.core.buttons.ResupplyButton;
 import net.stalemate.server.core.controller.Game;
+import net.stalemate.server.core.pathfinding.Pathfinding;
+import net.stalemate.server.core.properties.Properties;
+import net.stalemate.server.core.units.util.IBuilding;
 import net.stalemate.server.core.units.util.IMechanized;
 
 public class TransportPlane extends AirUnit implements IMechanized {
+
+    public class LoadButton implements ISelectorButtonUnit {
+        @Override
+        public String bind() {
+            return "L";
+        }
+
+        @Override
+        public String texture() {
+            return "texture_missing";
+        }
+
+        @Override
+        public String identifier() {
+            return "button_transport_plane_load_unit";
+        }
+
+        @Override
+        public void action(Unit selected_unit, Unit unit, Game gameController) {
+            if (loadedUnit == null && !hasTurnEnded && !(selected_unit instanceof IBuilding)){
+                gameController.rmEntity(selected_unit);
+                loadedUnit = selected_unit;
+                endTurn();
+            }
+        }
+
+        @Override
+        public int selector_range() {
+            return 1;
+        }
+
+        @Override
+        public String selector_texture() {
+            return "assets/ui/selectors/ui_move.png";
+        }
+
+        @Override
+        public boolean isUsedOnOurUnit() {
+            return true;
+        }
+
+        @Override
+        public boolean isUsedOnEnemy() {
+            return false;
+        }
+
+        @Override
+        public boolean isUsedOnAlliedUnit() {
+            return true;
+        }
+    }
+
+
+    public class DeployButton implements ISelectorButton{
+        @Override
+        public String bind() {
+            return "D";
+        }
+
+        @Override
+        public String texture() {
+            return "texture_missing";
+        }
+
+        @Override
+        public String identifier() {
+            return "button_transport_plane_deploy";
+        }
+
+        @Override
+        public void action(int x, int y, Unit unit, Game gameController) {
+            if (!hasTurnEnded)
+                if (Pathfinding.isCoordPassable(x, y, gameController) && loadedUnit != null) {
+                    loadedUnit.setX(x);
+                    loadedUnit.setY(y);
+                    gameController.addEntity(loadedUnit);
+                    loadedUnit = null;
+                }
+        }
+
+        @Override
+        public int selector_range() {
+            return 1;
+        }
+
+        @Override
+        public String selector_texture() {
+            return "assets/ui/selectors/ui_move.png";
+        }
+    }
+
+    private Unit loadedUnit = null;
+
     public TransportPlane(int x, int y, Game game) {
         super(x, y, game, new Unit.UnitStats(8, 8, 0, 2, 0, 0, 25, 30, 0, 0, 0), new AnimationController(), "Transport Plane");
         Animation animation = new Animation(1);
@@ -46,8 +141,31 @@ public class TransportPlane extends AirUnit implements IMechanized {
     public Unit.IButton[] getButtons() {
         IButton[] buttons = new IButton[9];
         buttons[0] = new MoveButton(movement_range, Layer.AIR);
+
+        if (loadedUnit == null){
+            buttons[6] = new LoadButton();
+        }
+        else
+            buttons[6] = new DeployButton();
+
         buttons[8] = new ResupplyButton();
         buttons[7] = new ResupplyButton(Layer.AIR);
         return buttons;
+    }
+
+    @Override
+    public void onDeath() {
+        super.onDeath();
+        if (loadedUnit != null) {
+            loadedUnit.setHp(-1);
+        }
+    }
+
+    @Override
+    public Properties getProperties() {
+        Properties p = super.getProperties();
+        if (loadedUnit != null)
+            p.put("ldunit", loadedUnit.getName());
+        return p;
     }
 }

66e5c6b398cb46ef30376a17e873546800b3a8e4
Weltspear <>
2023-06-18 13:09:28 +0200

    Added BattleGroup tags.

--- src/main/java/net/stalemate/server/core/ai/BattleGroup.java
+++ src/main/java/net/stalemate/server/core/ai/BattleGroup.java
@@ -422,4 +422,14 @@ public class BattleGroup {
     public void makeAutonomous(){
         isAutonomous = true;
     }
+
+    private String tag = null;
+
+    public void setTag(String tag){
+        this.tag = tag;
+    }
+
+    public String getTag(){
+        return tag;
+    }
 }
